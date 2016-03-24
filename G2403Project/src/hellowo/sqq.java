331c19561c0e6917b72a45cb87a6add78470534d
Mechan1c <slackhick@gmail.com>
2016-03-24 16:11:15 +0200

    improved sqq

--- G2403Project/src/hellowo/sqq.java
+++ G2403Project/src/hellowo/sqq.java
@@ -4,18 +4,15 @@ public class sqq
 {
 	public static void main(String[] args)
 	{
-		System.out.println(sqqr(5));
+		int x = 5;
+		int num = 1;
+		    int rez = 0;
+		    while(x>0){
+		        x-=num;
+		        num+=2;
+		        rez+=(x<0)?0:1;
+		    }
+		System.out.println(rez);
 	}
-
-
-public static int sqqr(int x){
-    int num = 1;
-    int rez = 0;
-    while(x>0){
-        x-=num;
-        num+=2;
-        rez+=(x<0)?0:1;
-    }
-        return rez;
-   }
 }
+
